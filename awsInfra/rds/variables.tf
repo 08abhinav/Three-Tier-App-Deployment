6c651e5c44b1b74cf8c1ee6845a5de9b8319195d
08abhinav <abhinavnegi101@gmail.com>
2026-04-02 15:45:50 +0530

    variables used in rds module

---
+++ awsInfra/rds/variables.tf
@@ -0,0 +1,34 @@
+variable "db_storage" {
+    description = "Storage required for db"
+    default = "change-with-your-value"
+}
+
+variable "db_name" {
+    description = "Database name"
+    default = "change-with-your-value"
+}
+
+variable "db_engine_version" {
+    description = "Database type"
+    default = "change-with-your-value"
+}
+
+variable "db_engine" {
+    description = "Database version"
+    default = "change-with-your-value"
+}
+
+variable "instance_class" {
+    description = "Instance class"
+    default = "change-with-your-value"
+}   
+
+variable "db_user" {
+    description = "Database username"
+    default = "change-with-your-value"
+}
+
+variable "db_password" {
+    description = "Database password"
+    default = "change-with-your-value"
+}
