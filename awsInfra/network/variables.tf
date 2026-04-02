e80a09383ddb3d7a319770539eb11ec1ebcbcdc9
08abhinav <abhinavnegi101@gmail.com>
2026-04-02 15:42:37 +0530

    variables used in network modules

---
+++ awsInfra/network/variables.tf
@@ -0,0 +1,41 @@
+variable "az1" {
+    description = "availability zone 1"
+    type = string
+    default = "change-with-your-value"
+}
+
+variable "az2" {
+    description = "availability zone 2"
+    type = string
+    default = "change-with-your-value"
+}
+
+variable "cidr_block" {
+    description = "cidr block for vpc"
+    type = string
+    default = "change-with-your-value"
+}
+
+variable "public_subnet1_cidr" {
+    description = "cidr block for public subnet1"
+    type = string
+    default = "change-with-your-value"
+}
+
+variable "public_subnet2_cidr" {
+    description = "cidr block for public subnet2"
+    type = string
+    default = "change-with-your-value"
+}
+
+variable "private_subnet1_cidr" {
+    description = "cidr block for private subnet1"
+    type = string
+    default = "change-with-your-value"
+}
+
+variable "private_subnet2_cidr" {
+    description = "cidr block for private subnet1"
+    type = string
+    default = "change-with-your-value"
+}
